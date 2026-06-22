ca1811c4b04ee2c844b5b09d4d6d249833dc9df1
Vaibhav Rathore <130953493+Vrathore03@users.noreply.github.com>
2026-06-22 22:09:12 +0530

    Change server URL to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Generate from './pages/Generate'
 import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://genweb-ai-797o.onrender.com"
  
 function App() {
   useGetCurrentUser()
