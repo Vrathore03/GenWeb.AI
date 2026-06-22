27c31a7d6a2ca9a1261e325a5da552b283956f1e
Vaibhav Rathore <130953493+Vrathore03@users.noreply.github.com>
2026-06-22 22:28:04 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://genweb-ai-1-zos3.onrender.com",
     credentials: true
 }))
 app.use("/api/auth", authRouter)
